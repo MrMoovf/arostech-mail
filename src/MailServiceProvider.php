97c261c1e19438a0f11adf1c0812b69c565010ca
MrMoovf <anderskozuch1998@gmail.com>
2024-04-12 13:54:17 +0200

    going back to when it worked omg

--- src/MailServiceProvider.php
+++ src/MailServiceProvider.php
@@ -13,9 +13,5 @@ class MailServiceProvider extends ServiceProvider {
     {
         $this->loadViewsFrom(__DIR__.'/views/emails', 'arostech');
 
-
-        $this->publishes([
-            __DIR__.'config/arostech-mail.php' => config_path('arostech-mail.php'),
-        ]);
     }
 }
--- src/config/arostech-mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'app_name' => env('APP_NAME'),
-];
