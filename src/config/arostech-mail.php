667c65067763f4cc9d25fbad784835a3846d09ea
MrMoovf <anderskozuch1998@gmail.com>
2024-05-10 17:25:11 +0200

    Update arostech-mail.php

--- src/config/arostech-mail.php
+++ src/config/arostech-mail.php
@@ -3,5 +3,7 @@
 return [
     'app_owner_name' => env('APP_OWNER_NAME'),
     'app_name' => env('APP_NAME'),
-    'app_url' => env('APP_URL')
+    'app_url' => env('APP_URL'),
+    'arostech_username' => env('AROSTECH_USERNAME'),
+    'arostech_password' => env('AROSTECH_PASSWORD'),
 ];
